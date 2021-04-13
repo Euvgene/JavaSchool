c15dee435b67bae96888c614bf1293422efa1bb6
Eugene <julianboznyakov@gmail.com>
2021-04-13 11:19:35 +0300

    DispatcherServlet & ViewResolver config

---
+++ src/main/java/com/evgenii/my_market/configs/SpringConfig.java
@@ -0,0 +1,32 @@
+package com.evgenii.my_market.configs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("com.evgenii.my_market")
+/*@PropertySource({"classpath:application.properties"})*/
+@RequiredArgsConstructor
+public class SpringConfig implements WebMvcConfigurer {
+
+    // define a bean for ViewResolver
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
